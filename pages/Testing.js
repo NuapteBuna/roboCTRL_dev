572ee014c9c473966c7b56e5d7ec1bc101e11f5c
Andrei9383 <andreistan275@gmail.com>
2022-10-11 21:21:48 +0300

    Fix buttons and alignments

--- pages/Testing.js
+++ pages/Testing.js
@@ -1,30 +1,30 @@
-import { Text } from "react-native-paper";
-import { Button } from "react-native-paper";
-import { View } from "react-native";
-import { useCallback, useContext, useState } from "react";
-import { KorolJoystick } from "korol-joystick-custom";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { withTheme } from "react-native-paper";
-import Header from "../components/Header/Header";
-import { useEffect, useRef } from "react";
+import {Text} from 'react-native-paper';
+import {Button} from 'react-native-paper';
+import {addons, View} from 'react-native';
+import {useCallback, useContext, useState} from 'react';
+import {KorolJoystick} from 'korol-joystick-custom';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {withTheme} from 'react-native-paper';
+import Header from '../components/Header/Header';
+import {useEffect, useRef} from 'react';
 
-import { Checkbox } from "react-native-paper";
-import { StateContext } from "../App";
+import {Checkbox} from 'react-native-paper';
+import {StateContext} from '../App';
 
-import BluetoothSerial from "react-native-bluetooth-serial-speedy";
+import BluetoothSerial from 'react-native-bluetooth-serial-speedy';
 
-import { IconButton, MD3Colors } from "react-native-paper";
+import {IconButton, MD3Colors} from 'react-native-paper';
 
-const Testing = (props) => {
-  const { colors } = props.theme;
+const Testing = props => {
+  const {colors} = props.theme;
 
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
 
-  const { posX, setPosX, posY, setPosY, move, setMove } =
+  const {posX, setPosX, posY, setPosY, move, setMove} =
     useContext(StateContext);
 
-  const debounce = (func) => {
+  const debounce = func => {
     let timer;
     return function (...args) {
       const context = this;
@@ -36,154 +36,203 @@ const Testing = (props) => {
     };
   };
 
-  const handleChangeX = (input) => {
+  const handleChangeX = input => {
     if (input != x) {
       setX(input);
     }
   };
   const optimizedX = useCallback(debounce(handleChangeX));
 
-  const handleChangeY = (input) => {
+  const handleChangeY = input => {
     if (input != y) {
       setY(input);
     }
   };
   const optimizedY = useCallback(debounce(handleChangeY));
 
-  const [dir, setDir] = useState("");
+  const [dir, setDir] = useState('');
 
-  const inInterval = (x, a, b) => {
-    if (x >= a && x <= b) return true;
-    return false;
-  };
-
-  let eroare = 10;
+  let error = 30;
+  let center = 70;
 
   useEffect(() => {
-    //TODO: CHANGE THIS (ONENOTE)
-    /*if (x > 50 + eroare && y > 50 + eroare) {
-      if (inInterval(x, 50, 65)) setDir("w");
-      else setDir("e");
-    } else if (x < 50 && y > 50) {
-      if (inInterval(x, 35, 50)) setDir("w");
-      else setDir("q");
-    } else if (x < 50 && y < 50) {
-      setDir("a");
-    } else if (x > 50 && y < 50) {
-      setDir("d");
-    }*/
+    if (x > center && y > center) {
+      if (x > center + error && y > center + error) {
+        setDir('e');
+      } else if (x > center + error) {
+        setDir('d');
+      } else if (y > center + error) {
+        setDir('w');
+      }
+    }
+
+    if (x < center && y > center) {
+      if (x < center - error && y > center + error) {
+        setDir('q');
+      } else if (x < center - error) {
+        setDir('a');
+      } else if (y > center + error) {
+        setDir('w');
+      }
+    }
+
+    if (x < center && y < center) {
+      if (x < center - error && y < center - error) {
+        setDir('z');
+      } else if (x < center - error) {
+        setDir('a');
+      } else if (y < center - error) {
+        setDir('x');
+      }
+    }
+
+    if (x > center && y < center) {
+      if (x > center + error && y < center - error) {
+        setDir('c');
+      } else if (x > center + error) {
+        setDir('d');
+      } else if (y < center - error) {
+        setDir('x');
+      }
+    }
+
     if (x == 0 && y == 0) {
-      setDir("s");
+      setDir('s');
     }
   }, [x, y]);
 
   const [checked, setChecked] = useState(false);
 
   useEffect(() => {
-    BluetoothSerial.write(dir).then((res) => console.log(res));
+    BluetoothSerial.write(dir).then(res => console.log(res));
   }, [dir]);
+
+  let buttonSize = 36;
 
   return (
     <>
       <Header title="Navigation" />
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <View
-          style={{
-            display: "flex",
-            flex: 1,
-            alignContent: "center",
-            alignItems: "center",
-            alignSelf: "center",
-            marginTop: "60%",
-          }}
-        >
-          <IconButton
-            icon="arrow-up"
-            size={20}
-            mode="contained"
-            onPressIn={() => setDir("w")}
-            onPress={() => setDir("w")}
-            onPressOut={() => setDir("s")}
-          ></IconButton>
-
-          <IconButton
-            icon="arrow-down"
-            size={20}
-            mode="contained"
-            onPressIn={() => setDir("x")}
-            onPress={() => setDir("x")}
-            onPressOut={() => setDir("s")}
-          ></IconButton>
-
-          <IconButton
-            icon="arrow-top-right"
-            size={20}
-            mode="contained"
-            onPressIn={() => setDir("e")}
-            onPress={() => setDir("e")}
-            onPressOut={() => setDir("s")}
-          ></IconButton>
-
+      <View
+        style={{
+          display: 'flex',
+          flex: 1,
+          alignContent: 'center',
+          alignItems: 'center',
+          alignSelf: 'center',
+          marginTop: '40%',
+        }}>
+        <View style={{flexDirection: 'row', display: 'flex'}}>
           <IconButton
             icon="arrow-top-left"
-            size={20}
+            size={buttonSize}
             mode="contained"
-            onPressIn={() => setDir("q")}
-            onPress={() => setDir("q")}
-            onPressOut={() => setDir("s")}
-          ></IconButton>
-
+            onPressIn={() => setDir('q')}
+            onPress={() => setDir('s')}
+          />
           <IconButton
-            icon="arrow-bottom-left"
-            size={20}
+            icon="arrow-up"
+            size={buttonSize}
             mode="contained"
-            onPressIn={() => setDir("z")}
-            onPress={() => setDir("z")}
-            onPressOut={() => setDir("s")}
-          ></IconButton>
-
+            onPressIn={() => setDir('w')}
+            onPress={() => setDir('s')}
+          />
           <IconButton
-            icon="arrow-bottom-right"
-            size={20}
+            icon="arrow-top-right"
+            size={buttonSize}
             mode="contained"
-            onPressIn={() => setDir("c")}
-            onPress={() => setDir("c")}
-            onPressOut={() => setDir("s")}
-          ></IconButton>
-
+            onPressIn={() => setDir('e')}
+            onPress={() => setDir('s')}
+          />
+        </View>
+        <View style={{flexDirection: 'row'}}>
           <IconButton
             icon="arrow-left"
-            size={20}
+            size={buttonSize}
             mode="contained"
-            onPressIn={() => setDir("a")}
-            onPress={() => setDir("a")}
-            onPressOut={() => setDir("s")}
-          ></IconButton>
-
+            onPressIn={() => setDir('a')}
+            onPress={() => setDir('s')}
+          />
+          <IconButton
+            icon="rotate-left"
+            size={buttonSize / 2}
+            style={{
+              margin: 0,
+              alignContent: 'center',
+              alignItems: 'center',
+              alignSelf: 'center',
+            }}
+            mode="contained"
+            onPressIn={() => setDir('h')}
+            onPress={() => setDir('s')}
+          />
+          <IconButton
+            icon="rotate-right"
+            size={buttonSize / 2}
+            style={{
+              margin: 0,
+              alignContent: 'center',
+              alignItems: 'center',
+              alignSelf: 'center',
+            }}
+            mode="contained"
+            onPressIn={() => setDir('j')}
+            onPress={() => setDir('s')}
+          />
           <IconButton
             icon="arrow-right"
-            size={20}
+            size={buttonSize}
             mode="contained"
-            onPressIn={() => setDir("d")}
-            onPress={() => setDir("d")}
-            onPressOut={() => setDir("s")}
-          ></IconButton>
-
+            onPressIn={() => setDir('d')}
+            onPress={() => setDir('s')}
+          />
+        </View>
+        <View style={{flexDirection: 'row'}}>
+          <IconButton
+            icon="arrow-bottom-left"
+            size={buttonSize}
+            mode="contained"
+            onPressIn={() => setDir('z')}
+            onPress={() => setDir('s')}
+          />
+          <IconButton
+            icon="arrow-down"
+            size={buttonSize}
+            mode="contained"
+            onPressIn={() => setDir('x')}
+            onPress={() => setDir('s')}
+          />
+          <IconButton
+            icon="arrow-bottom-right"
+            size={buttonSize}
+            mode="contained"
+            onPressIn={() => setDir('c')}
+            onPress={() => setDir('s')}
+          />
+        </View>
+      </View>
+      <GestureHandlerRootView style={{flex: 1}}>
+        <View
+          style={{
+            display: 'flex',
+            flex: 1,
+            alignContent: 'center',
+            alignItems: 'center',
+            alignSelf: 'center',
+          }}>
           <Text>hello world</Text>
           <Checkbox
-            status={checked ? "checked" : "unchecked"}
+            status={checked ? 'checked' : 'unchecked'}
             onPress={() => {
               setChecked(!checked);
               setMove(!checked);
-            }}
-          ></Checkbox>
+            }}></Checkbox>
           <KorolJoystick
-            //color={colors.secondary}
-            radius={75}
-            onMove={(data) => (
+            color={colors.primary}
+            radius={100}
+            onMove={data => (
               optimizedX(data.position.x), optimizedY(data.position.y)
             )}
-            onStop={(data) => (
+            onStop={data => (
               optimizedX(data.position.x), optimizedY(data.position.y)
             )}
           />
@@ -191,7 +240,6 @@ const Testing = (props) => {
         <Text>{parseInt(x)}</Text>
         <Text>{parseInt(y)}</Text>
         <Text>{dir}</Text>
-        <Text>{counter}</Text>
       </GestureHandlerRootView>
     </>
   );
